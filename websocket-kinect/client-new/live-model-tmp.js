3eaa4aed221930dad23821deabe6956f924f7df1
Craig Tanis <craig-tanis@utc.edu>
2013-02-28 11:19:03 -0500

    polyognal faces now viewable

--- websocket-kinect/client-new/live-model-tmp.js
+++ websocket-kinect/client-new/live-model-tmp.js
@@ -8,8 +8,8 @@ LiveModel = function() {
     var offw = inputW/2;
 
 
-    var vh = 100;		// defines number of faces
-    var vw = 100;
+    var vh = 200;		// defines number of faces
+    var vw = 300;
 
 
     var dy = inputH / vh;
@@ -22,25 +22,21 @@ LiveModel = function() {
 
     var vertices;
     var i,j
-    var usualY=[];
+    // var usualY=[];
 
-    for (j=0; j< vh; j++) {
+    for (j=vh-1; j>=0; j--) {	// server loads points bottom to top
 	for (i=0; i<vw; i++) {
 
 	    var x = ((i*dx)-offw)*2;
 	    var y = ((j*dy)-offh)*2;
 
 	    model.vertices.push(new THREE.Vector3(x,y,0));
-	    usualY.push(y);
+//	    usualY.push(y);
 	}
     }
 
     var vertexCount = model.vertices.length;
 
-    // model.vertices.push( new THREE.Vector3( -10,  10, 0 ) );
-    // model.vertices.push( new THREE.Vector3( -10, -10, 0 ) );
-    // model.vertices.push( new THREE.Vector3(  10, -10, 0 ) );
-    // model.faces.push( new THREE.Face3( 0, 1, 2 ) );
     
     for (j=0; j<vh-1; j++) {
 	for (i=0; i<vw-1; i++) {
@@ -51,13 +47,17 @@ LiveModel = function() {
 	    var d = (j+1)*vw+i+1;
 
 	    model.faces.push( new THREE.Face3(a,b,c) );
-	    model.faces.push( new THREE.Face3(c,d,b) );
+	    model.faces.push( new THREE.Face3(c,b,d) );
 	}
     }
 
-//    model.computeBoundingSphere();
+    // model.computeBoundingSphere();
 
-    var faceMesh = new THREE.Mesh(model);
+    var material = new THREE.MeshBasicMaterial();
+    material.wireframe=false;
+
+    //var faceMesh = new THREE.Mesh(model, material);
+    var faceMesh = new THREE.Mesh(model, material,true);
 
     this.sceneContents = function() {
 	return faceMesh;
@@ -77,6 +77,7 @@ LiveModel = function() {
 
     	pd = [bytes[1], bytes[2], bytes[3], bytes[4]];
 
+
     	var pIdx = 0;
     	var byteIdx = 5;
     	var rgbByteIdx = numData+5;
@@ -84,84 +85,57 @@ LiveModel = function() {
 
 	var v=0;
 	var x,y;
+	var skip = Math.floor(dy)*inputW;
 
-	for (y=0; y<inputH; y+= dy) {
-	    for (x=0; x<inputW; x+= dx) {
+	for (y=0; y<vh; y++) {
+	    for (x=0; x<vw; x++) {
+		
+		var byteOffset = Math.floor(x*dx)
 
-		var abyte= bytes[byteIdx+Math.floor(x)];
-		var vtx = model.vertices[v];
-
-		if (abyte == 255) {
-		    vtx.setY(-5000);
-		}
-		else {
-		    var depth = (128 - abyte)*10;
-		    vtx.setZ(depth);
-		    vtx.setY(usualY[v]);
-		}
-
+		var abyte= bytes[byteIdx+byteOffset];
+		
+		var depth = (128 - abyte)*10;
+		
+		model.vertices[v].setZ(depth);
 		v = v+1;
 	    }
 	    
-	    byteIdx+=inputW;
+	    byteIdx+=skip;
+	    rgbByteIdx+=skip;
 	}
+
+
+	// console.log("v: " + v +"numVerts: "+numVerts+"numData: " + numData);
+	// console.log("post set: " + byteIdx + "," + numData+5);
 	    
 
+	// works when inputH,inputW = vh,vw
+	// for (y=0; y<inputH; y++) {
+	//     for (x=0; x<inputW; x++) {
 
-	// var c=byteIdx;
-	// for (j=0; j< vh; j++) {
-	//     for (i=0; i<vw; i++) {
-	// 	var abyte = bytes[c+i*dx];
-	// 	var vtx = model.vertices[j*vw+i];
+	// 	var abyte= bytes[byteIdx];
+	// 	var vtx = model.vertices[v];
 
-	// 	if (abyte == 255) {
-	// 	    vtx.setY(-5000);
-	// 	}
-	// 	else {
-	// 	    var depth = (128-abyte)*10;
-	// 	    vtx.setY(usualY[j*vw+i]);
-	// 	    vtx.setZ(depth);
-	// 	}
-	//     }	    
+	// 	// if (abyte == 255) {
+	// 	//     vtx.setY(-5000);
+	// 	// }
+	// 	// else {
+	// 	//     var depth = (128 - abyte)*10;
+	// 	//     vtx.setZ(depth);
+	// 	//     vtx.setY(usualY[v]);
+	// 	// }
 
-	//     c += inputW;
+	// 	var depth = (128 - abyte)*10;
+	// 	vtx.setZ(depth);
+
+	// 	v++;
+	// 	byteIdx++;
+	//     }
 	// }
 
 
-	// var i=0;
-	// for (c=0; c<=numData; c += dx) {
-	//     var depth = 128 - bytes[c+byteIdx];
-	//     model.vertices[i]=new  THREE.vector3(depth*10);
-	//     i++;
-	// }
-
-
-    	// for (y = 0; 0 <= h ? y < h : y > h; 0 <= h ? y++ : y--) {
-        //     for (x = 0; 0 <= w ? x < w : x > w; 0 <= w ? x++ : x--) {
-    	// 	pv = pvs[pIdx];
-    	// 	aByte = bytes[byteIdx];
-	
-    	// 	if (aByte === 255) {
-    	// 	    pv.position.y = -5000;
-    	// 	} else {
-    	// 	    pv.position.y = pv.usualY;
-    	// 	    depth = 128 - aByte;
-    	// 	    pv.position.z = depth * 10;
-    	// 	}
-
-    	// 	particles.colors[pIdx].r = bytes[rgbByteIdx+0]/ 256;
-    	// 	particles.colors[pIdx].g = bytes[rgbByteIdx+1]/ 256;
-    	// 	particles.colors[pIdx].b = bytes[rgbByteIdx+2]/ 256;
-
-	
-    	// 	rgbByteIdx += 3;
-    	// 	pIdx += 1;
-    	// 	byteIdx += 1;
-        //     }
-    	// }
 
     	model.verticesNeedUpdate = true;
-    	model.colorsNeedUpdate = true;
     	return true;
     };
 
