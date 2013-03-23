17455426b171e08dd4d74c407d2157ac34ffc4c8
Forrest Pruitt <forrest-pruitt@mocs.utc.edu>
2013-03-23 18:39:06 -0500

    Cleaned up cloud server code, added a few comments.

--- websocket-kinect/server/newCloudServer.js
+++ websocket-kinect/server/newCloudServer.js
@@ -1,3 +1,12 @@
+/*---------------------------------------------------------
+newCloudServer.js
+    A Pass-through server for Engage3D's Kinect Video 
+    Conferencing App. Creates two servers to listen for
+    messages from both the Viewer and the Kincet.
+Author: Forrest Pruitt
+---------------------------------------------------------*/
+
+
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 var http2 = require('http');
@@ -11,20 +20,10 @@ var viewerServer = http2.createServer(function(request, response) {
     // we don't have to implement anything.
 });
 
-
-//var server2 = http.createServer(function(request, response) {
-    // process HTTP request. Since we're writing just WebSockets server
-    // we don't have to implement anything.
-//});
-
 kinectServer.listen(9000, function() { });
 viewerServer.listen(9001, function() { });
 
-
 // create the server
-
-
-
 
 wsKinectServer = new WebSocketServer({
     httpServer: kinectServer,
@@ -40,15 +39,16 @@ wsViewerServer = new WebSocketServer({
     protocol: 'of-protocol'
 });
 
-
-
 var kinectConnection;
 var viewerConnection;
 var kinectConnected=false;
 var viewerConnected=false;
 
-// WebSocket server
-wsKinectServer.on('request', function(request) {
+//---------------------------------------------
+// Kinect Code
+//---------------------------------------------
+wsKinectServer.on('request', function(request) 
+{
     kinectConnection = request.accept('of-protocol', request.origin);
     connections.push(kinectConnection);
     console.log('Connected to kinect client.');
@@ -56,68 +56,72 @@ wsKinectServer.on('request', function(request) {
     // This is the most important callback for us, we'll handle
     // all messages from users here.
 
-    kinectConnection.on('message', function(message) {
-        if (message.type === 'utf8') {
+    kinectConnection.on('message', function(message) 
+    {
+        if (message.type === 'utf8') 
             console.log('Received Message: ' + message.utf8Data);
-        }
-        else if (message.type === 'binary') {
+
+        else if (message.type === 'binary') 
+        {
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            if(viewerConnected){
-                //console.log('Sending some shit to the viewer.');
-                connections.forEach(function(destination) {
+            if(viewerConnected)
+            {
+                connections.forEach(function(destination) 
+                {
                     if(destination != kinectConnection)
                         destination.sendBytes(message.binaryData);
                 });
-                //kinectConnection.sendBytes(message.binaryData);
             }
-            else
-                console.log('No viewer connected.');
         }
     });
-    /*
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            .sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-*/
 
-    kinectConnection.on('close', function(connection) {
-        // close user connection
+    kinectConnection.on('close', function(connection) 
+    {
         console.log('Closing connection to Kinect...');
     });
+
 });
 
+//-----------------------------------------------
+// Viewer Code
+//----------------------------------------------
 
-
-wsViewerServer.on('request', function(request) {
+//Set up the connection from this server to the viewer.
+wsViewerServer.on('request', function(request) 
+{
     clientConnection = request.accept(null, request.origin);
     connections.push(clientConnection);
     console.log('Connected to viewer client.');
     viewerConnected = true;
-    // This is the most important callback for us, we'll handle
-    // all messages from users here.
-    
-    clientConnection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            //console.log('Received Message: ' + message.utf8Data);
 
-            connections.forEach(function(destination) {
+    //Here we are handling messages from the viewer to the Kinect.
+    //This is currently (March 2013) used primarily for throttling the
+    //Stream, ie setting the framerate.
+    clientConnection.on('message', function(message) 
+    {
+        if (message.type === 'utf8') 
+        {
+            connections.forEach(function(destination) 
+            {
                 if(destination != clientConnection)
-                    //console.log('Sending message '+message.utf8Data+' to Kinect Server...')
                     destination.sendUTF(message.utf8Data);
-                });
+            });
         }
-        else if (message.type === 'binary') {
-            //console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+        else if (message.type === 'binary') 
+        {
+            destination.sendBytes(message.binaryData);
+            connections.forEach(function(destination) 
+            {
+                // 
+                if(destination != clientConnection)
+                    destination.sendBytes(message.binaryData);
+        
+            });
+            
         }
         console.log(message);
     });
+
     clientConnection.on('close', function(connection) {
         console.log('Closed connection to viewer.');
     });
